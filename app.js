3bd92848487136463a50a35e2eabe9581586f538
JefersonCalderon <jcalderon-2019036@kinal.edu.gt>
2021-08-27 08:59:34 -0600

    se agrego las cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser")
+var cors = require('cors')
 
 //Rutas
 var UsuariosRutas = require("./src/rutas/rutas.usuario")
@@ -13,9 +14,12 @@ var LibrosRutas = require("./src/rutas/rutas.libros")
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+//cors
+app.use(cors())
 
 app.use('/api', UsuariosRutas);
 app.use("/api", LibrosRutas);
+
 
 //Exportar
 module.exports = app;
